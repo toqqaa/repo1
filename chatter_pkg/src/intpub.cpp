eec02ebc0a3af9bd04e2bafe268d1e01ada92543
Toka Essam <145803764+toqqaa@users.noreply.github.com>
2024-01-26 17:20:15 +0200

    Add files via upload

---
+++ chatter_pkg/src/intpub.cpp
@@ -0,0 +1,42 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "int_pub_node");
+
+  
+  ros::NodeHandle n;
+
+  
+  ros::Publisher int_pub = n.advertise<std_msgs::Int32>("integer", 1000);
+
+  ros::Rate loop_rate(10);
+
+
+  int count = 20;
+  while (ros::ok()&& count<=100)
+  {
+    
+    std_msgs::Int32 int_msg;
+   int_msg.data = count;
+
+    ROS_INFO("%i", int_msg.data);
+
+
+   int_pub.publish(int_msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
